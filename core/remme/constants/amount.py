4434742bf934e158e6f6a35a76151bf1ade79894
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-11-22 16:36:09 +0200

    Create send tokens and check balance

---
+++ core/remme/constants/amount.py
@@ -0,0 +1,6 @@
+"""
+Provide constants for Remme amounts.
+
+Code below is vendor code and located by address `https://github.com/Remmeauth/remme-client-python`.
+"""
+STABLE_REMME_TOKENS_REQUEST_AMOUNT = 100
