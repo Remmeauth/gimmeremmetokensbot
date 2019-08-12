d784dd6eb608cf3ea9e46f3ed0f94adcd86ee717
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-08-12 20:26:24 +0300

    hot fix balances

--- src/eos/account.py
+++ src/eos/account.py
@@ -33,22 +33,46 @@ class Account:
         """
         Get balance of the account.
         """
+        # payload = {
+        #     'account': name,
+        #     'code': 'rem.token',
+        #     'symbol': symbol,
+        # }
         payload = {
-            'account': name,
-            'code': 'rem.token',
-            'symbol': symbol,
+            'account_name': name,
         }
 
-        response = requests.post(
-            NODEOS_API_URL + 'chain/get_currency_balance',
-            data=json.dumps(payload),
-            headers=APPLICATION_JSON_HEADERS,
-        )
-
         try:
-            return response.json().pop(0).replace(f' {symbol}', '')
-        except (KeyError, IndexError):
-            return '0.0000'
+            response = requests.post(
+                NODEOS_API_URL + 'chain/get_account',
+                data=json.dumps(payload),
+                headers=APPLICATION_JSON_HEADERS,
+            )
+
+            logger.info(f'AAA*50, {response.json()}')
+
+            core_liquid_balance = int(response.json().get('core_liquid_balance').split('.')[0])
+            staked = str(response.json().get('voter_info').get('staked'))[:6]
+
+            logger.info(f'{core_liquid_balance}')
+            logger.info(f'{staked}')
+
+            return f'{int(core_liquid_balance) + int(staked)}.0000 REM'
+
+        except AttributeError:
+            return '300000000.0000'
+
+        # response = requests.post(
+        #     NODEOS_API_URL + 'chain/get_currency_balance',
+        #     data=json.dumps(payload),
+        #     headers=APPLICATION_JSON_HEADERS,
+        # )
+
+        # try:
+        #     return response.json().pop(0).replace(f' {symbol}', '')
+        # except (KeyError, IndexError):
+        #     return '0.0000'
+        # return '00.0000'
 
     def create(self, wallet_public_key, name, symbol, stake_quantity):
         """
